0c8182a186e5062f4c00dcd2e519d301eaaa2213
Jorge Jimenez <georgeguitarra07@gmail.com>
2021-08-23 20:07:39 +0200

    User model added to django admin panel

--- app/core/admin.py
+++ app/core/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from .models import User
 
-# Register your models here.
+class UserAdmin(BaseUserAdmin):
+    ordering = ['id']
+    list_display = ['email', 'name']
+
+admin.site.register(User, UserAdmin)
