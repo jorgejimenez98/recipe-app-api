b512427cbee1fec41076ab5d7e3c538115a8261d
Jorge Jimenez <georgeguitarra07@gmail.com>
2021-08-23 19:27:50 +0200

    Added normalize_email method to create user method

--- app/core/models.py
+++ app/core/models.py
@@ -6,7 +6,7 @@ class UserManager(BaseUserManager):
 
     def create_user(self, email, password=None, **extra_fields):
         """ Create and saves a new User """
-        user = self.model(email=email, **extra_fields)
+        user = self.model(email=self.normalize_email(email), **extra_fields)
         user.set_password(password)
         user.save(using=self._db)
         return user 
