dae4104f81fc5e67f9018726d9e533cfccceea31
Jorge Jimenez <georgeguitarra07@gmail.com>
2021-08-23 19:47:38 +0200

    Added method to create a superUser

--- app/core/models.py
+++ app/core/models.py
@@ -13,6 +13,14 @@ class UserManager(BaseUserManager):
         user.save(using=self._db)
         return user 
     
+    def create_superuser(self, email, password):
+        """ Create a new Super User """
+        user = self.create_user(email, password)
+        user.is_staff = True 
+        user.is_superuser = True 
+        user.save(using=self._db)
+        return user 
+    
 
 class User(AbstractBaseUser, PermissionsMixin):
 
