2cbb59045b1023a8f44634207f03f16d13e1e7c1
Jorge Jimenez <georgeguitarra07@gmail.com>
2021-08-23 20:08:16 +0200

    Unit Test added to user model tha users are listed on users page

---
+++ app/core/tests/test_admin.py
@@ -0,0 +1,26 @@
+from django.test import TestCase, Client
+from django.contrib.auth import get_user_model
+from django.urls import reverse
+
+
+class AdminSiteTest(TestCase):
+    def setUp(self):  # Function tha will run berofe the tests
+        self.client = Client()
+        self.admin_user = get_user_model().objects.create_superuser(
+            email='test@gmail.com',
+            password='test123'
+        )
+        self.client.force_login(self.admin_user)
+        self.user = get_user_model().objects.create_user(
+            email='test1@gmail.com',
+            password='test12332',
+            name='User Name'
+        )
+
+    def test_users_listed(self):
+        """ Test that user are listed on user page """
+        url = reverse('admin:core_user_changelist')
+        res = self.client.get(url)
+
+        self.assertContains(res, self.user.name)
+        self.assertContains(res, self.user.email)
