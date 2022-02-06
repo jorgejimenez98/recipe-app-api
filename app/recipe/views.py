c059f552af97f8f9c15a55a386066ffeed1e17f0
Jorge Jimenez <georgeguitarra07@gmail.com>
2022-02-06 22:43:31 +0100

    Create Ingredient view set

--- app/recipe/views.py
+++ app/recipe/views.py
@@ -2,7 +2,7 @@ from rest_framework import viewsets, mixins
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.permissions import IsAuthenticated
 
-from .serializers import Tag, TagSerializer
+from .serializers import Tag, Ingredient, TagSerializer, IngredientSerializer
 
 
 class TagsViewSet(viewsets.GenericViewSet,
@@ -21,3 +21,16 @@ class TagsViewSet(viewsets.GenericViewSet,
     def perform_create(self, serializer):
         """ Create a new Tag """
         serializer.save(user=self.request.user)
+
+
+class IngredientViewSet(viewsets.GenericViewSet,
+                        mixins.ListModelMixin):
+    """ Manage Ingredients in the database """
+    authentication_classes = [TokenAuthentication, ]
+    permission_classes = [IsAuthenticated, ]
+    queryset = Ingredient.objects.all().order_by('-name')
+    serializer_class = IngredientSerializer
+
+    def get_queryset(self):
+        """ Return object for the current auth user only """
+        return self.queryset.filter(user=self.request.user).order_by('-name')
