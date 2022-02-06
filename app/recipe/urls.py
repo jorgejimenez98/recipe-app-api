42ec37c3510f5cd6b23581d5b1870e8b9774dcc5
Jorge Jimenez <georgeguitarra07@gmail.com>
2022-02-06 22:43:46 +0100

    Register view set on routers

--- app/recipe/urls.py
+++ app/recipe/urls.py
@@ -2,10 +2,11 @@ from django.urls import path
 from django.conf.urls import include
 from rest_framework.routers import DefaultRouter
 
-from .views import TagsViewSet
+from .views import TagsViewSet, IngredientViewSet
 
 router = DefaultRouter()
 router.register('tags', TagsViewSet)
+router.register('ingredients', IngredientViewSet)
 
 app_name = 'recipe' # this is for test reconize reverse urls
 
