291832bda211d0704c32e04c3bf3539aac518ae1
Jorge Jimenez <georgeguitarra07@gmail.com>
2022-02-06 21:29:27 +0100

    Create tag serializer

--- app/core/serializers.py
+++ app/core/serializers.py
@@ -1,8 +1,16 @@
 from rest_framework import serializers
-from .models import Ingredient
+from .models import Ingredient, Tag
 
 
 class IngredientSerializer(serializers.ModelSerializer):
     class Meta:
         model = Ingredient
-        fields = '__all__'
+        fields = ['id', 'name']
+        read_only_fields = ['id', ]
+
+
+class TagSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Tag
+        fields = ['id', 'name']
+        read_only_fields = ['id', ]
