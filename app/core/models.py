4806a224f12a6b904bb7ad7e71e633d8b912a6de
Jorge Jimenez <georgeguitarra07@gmail.com>
2022-02-06 19:55:35 +0100

    Create Ingredients Model

--- app/core/models.py
+++ app/core/models.py
@@ -1,6 +1,8 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 from django.contrib.auth.models import AbstractBaseUser, BaseUserManager,\
     PermissionsMixin
+
 
 class UserManager(BaseUserManager):
 
@@ -11,16 +13,16 @@ class UserManager(BaseUserManager):
         user = self.model(email=self.normalize_email(email), **extra_fields)
         user.set_password(password)
         user.save(using=self._db)
-        return user 
-    
+        return user
+
     def create_superuser(self, email, password):
         """ Create a new Super User """
         user = self.create_user(email, password)
-        user.is_staff = True 
-        user.is_superuser = True 
+        user.is_staff = True
+        user.is_superuser = True
         user.save(using=self._db)
-        return user 
-    
+        return user
+
 
 class User(AbstractBaseUser, PermissionsMixin):
 
@@ -29,6 +31,15 @@ class User(AbstractBaseUser, PermissionsMixin):
     name = models.CharField(max_length=255)
     is_active = models.BooleanField(default=True)
     is_staff = models.BooleanField(default=False)
-    
+
     objects = UserManager()
     USERNAME_FIELD = 'email'
+
+
+class Ingredient(models.Model):
+    """ Ingredient to be used in  arecipe """
+    name = models.CharField(max_length=255)
+    user = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
