6dbad89ffcdce69eb2306e37e5413c68e113d147
Jorge Jimenez <georgeguitarra07@gmail.com>
2022-02-06 22:27:13 +0100

    Create test create tag invalid

--- app/recipe/tests/test_tags_api.py
+++ app/recipe/tests/test_tags_api.py
@@ -77,3 +77,9 @@ class PrivateTagsApiTest(TestCase):
         ).exists()
 
         self.assertTrue(exist)
+
+    def test_create_tag_invalid(self):
+        payload = {'name': ''}
+        res = self.client.post(TAGS_URL, payload)
+
+        self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
