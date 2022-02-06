66705f6de68e10040eef6e154d14e8d2bf0e2221
Jorge Jimenez <georgeguitarra07@gmail.com>
2022-02-06 21:36:06 +0100

    Create sample user func, test_tag_str test

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -1,6 +1,11 @@
 from django.test import TestCase
 from django.contrib.auth import get_user_model
-from ..models import Ingredient
+from ..models import Ingredient, Tag
+
+
+def sample_user(email='test@gmail.com', password='testpass'):
+    """ Create a sample user """
+    return get_user_model().objects.create_user(email, password)
 
 
 class ModelTests(TestCase):
@@ -59,3 +64,12 @@ class ModelTests(TestCase):
         )
 
         self.assertEqual(str(ingredient), ingredient.name)
+
+    def test_tag_str(self):
+        """ Test the tag string representation """
+        tag = Tag.objects.create(
+            user=sample_user(),
+            name='Vegan'
+        )
+
+        self.assertEqual(str(tag), tag.name)
