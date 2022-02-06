aa554d0b38ff958983e609f3dd7d444be94d135a
Jorge Jimenez <georgeguitarra07@gmail.com>
2022-02-06 22:08:20 +0100

    Create tags view set

--- app/recipe/views.py
+++ app/recipe/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from rest_framework import viewsets, mixins
+from rest_framework.authentication import TokenAuthentication
+from rest_framework.permissions import IsAuthenticated
 
-# Create your views here.
+from .serializers import Tag, TagSerializer
+
+
+class TagsViewSet(viewsets.GenericViewSet, mixins.ListModelMixin):
+    """ Manage tags in the database """
+    authentication_classes = [TokenAuthentication, ]
+    permission_classes = [IsAuthenticated, ]
+    queryset = Tag.objects.all().order_by('-name')
+    serializer_class = TagSerializer
