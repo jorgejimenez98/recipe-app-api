a1004e09dec171048f25b3ac342fd735824edd3f
Jorge Jimenez <georgeguitarra07@gmail.com>
2022-02-06 23:10:06 +0100

    Add Recipe model to amdin

--- app/core/admin.py
+++ app/core/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
-from .models import User, Ingredient, Tag
+from .models import User, Ingredient, Tag, Recipe
 from django.utils.translation import gettext as _
 
 
@@ -27,3 +27,4 @@ class UserAdmin(BaseUserAdmin):
 admin.site.register(User, UserAdmin)
 admin.site.register(Ingredient)
 admin.site.register(Tag)
+admin.site.register(Recipe)
