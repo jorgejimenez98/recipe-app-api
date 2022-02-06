1e9b1c916c19d2a7869cbbede1e87c17d7272269
Jorge Jimenez <georgeguitarra07@gmail.com>
2022-02-06 21:29:16 +0100

    Create Tag Model

--- app/core/models.py
+++ app/core/models.py
@@ -36,6 +36,18 @@ class User(AbstractBaseUser, PermissionsMixin):
     USERNAME_FIELD = 'email'
 
 
+class Tag(models.Model):
+    """Tag to be used for a recipe"""
+    name = models.CharField(max_length=255)
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+    )
+
+    def __str__(self):
+        return self.name
+
+
 class Ingredient(models.Model):
     """ Ingredient to be used in  arecipe """
     name = models.CharField(max_length=255)
